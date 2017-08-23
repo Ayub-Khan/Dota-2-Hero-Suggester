4be965dd4fbfe2c3d5e459b5d87149db5eb0c97f
ayub-khan <muhammadayubkhan6@gmail.com>
2017-08-23 12:20:35 +0500

    Updated Matchups data and added Lane Data in hero Data and pulled its data

---
+++ README.rst
@@ -0,0 +1,7 @@
+Dota World App
+==============
+
+This Repo is reponsible to carry out all the dirty work of the Dota 2 Hero Suggester Web app and provide it with all the data. 
+
+==========================
+
--- hero_data_getter.py
+++ hero_data_getter.py
@@ -5,31 +5,6 @@ import os
 class HeroDataGetterOne(scrapy.Spider):
     name = 'matchups_getter'
 
-    # def start_requests(self):
-    #     with open('heros.txt', 'r') as f:
-    #         for line in f:
-    #             line = line.strip().replace(" ", "_").replace("'", "%27")
-    #             yield scrapy.Request(u'https://dota2.gamepedia.com/' + line,
-    #                                  self.parse)
-    #
-    # def parse(self, response):
-    #     file = open("hero_data.txt", "a+")
-    #     name = (response.css('title::text').extract()[0]).split('-')[0].strip()
-    #     data = response.css('div.mw-normal-catlinks').css('a::text').extract()[2:]
-    #
-    #     hero_type = data[0].split(' ')[0]
-    #     attack_type = data[1].split(' ')[0]
-    #     roles = ""
-    #     for item in data[2:]:
-    #         if item == 'Carries':
-    #             roles += '-Carry'
-    #         elif item.endswith('s'):
-    #             roles += '-' + item[:-1]
-    #         else:
-    #             roles += '-' + item
-    #     file.write(name + '-' + hero_type + '-' + attack_type + roles + '\n')
-    #     file.close()
-
     def start_requests(self):
         with open('heros.txt', 'r') as f:
             for line in f:
@@ -41,18 +16,17 @@ class HeroDataGetterOne(scrapy.Spider):
         file = open("hero_data.txt", "a+")
         name = (response.css('title::text').extract()[0]).split('-')[0].strip()
         data = response.css("div.header-content-title").css("small::text").extract()[0].split(',')
-
         hero_type = ""
         if response.css("tbody.primary-agility").extract() != []:
             hero_type = "Agility"
         elif response.css("tbody.primary-intelligence").extract() != []:
             hero_type = "Intelligence"
-        else :
+        else:
             hero_type = "Strength"
-
+        lane_details = str(response.css('tbody')[1].css('tr').css('td::text').extract())
         attack_type = data[0]
         roles = ""
         for item in data[1:]:
             roles += '-' + item.strip()
-        file.write(name + '-' + hero_type + '-' + attack_type + roles + '\n')
+        file.write(name + '-' + hero_type + '-' + attack_type + roles + '-' + lane_details + '\n')
         file.close()
